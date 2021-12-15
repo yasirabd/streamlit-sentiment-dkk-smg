cbbf61edb91f39aac95e52a9e1410fa3b09acf7a
yasirabd <sir.yasirabd@gmail.com>
2021-12-15 18:59:49 +0700

    modify sidebar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 from menu_home import display_home
 from menu_data_collection import display_data_collection
 from menu_data_annotation import display_data_annotation
@@ -15,6 +16,26 @@ st.set_page_config(layout="wide")
 list_menu = ['Home', 'Data Collection', 'Data Annotation', 'Exploratory Data Analysis', 'Data Preprocessing', 
              'Feature Extraction', 'Modeling', 'Inference', 'Deployment']
 menu_choice = st.sidebar.selectbox("Select a menu", list_menu)
+st.sidebar.title('Teams')
+text = """
+1. Alfi Fauzia Hanifah ([LinkedIn](https://www.linkedin.com/in/alfifauziahanifah/) / [IG](https://www.instagram.com/alfifao/))
+2. Annisa P A ([LinkedIn](https://www.linkedin.com/in/annisapa/) / [IG](https://www.instagram.com/annisapa__/))
+3. Javas Alfreda Belva ([LinkedIn](https://www.linkedin.com/in/javasalfredabyp/) / [IG](https://www.instagram.com/javasalfreda_byp/))
+4. Muhammad Aghassi ([LinkedIn](https://www.linkedin.com/in/maghassiz/) / [IG](https://www.instagram.com/maghassiz/))
+5. Yasir Abdur Rohman ([LinkedIn](https://www.linkedin.com/in/yasirabd/) / [IG](https://www.instagram.com/yasirabdr/))
+
+---
+
+"""
+st.sidebar.markdown(text, unsafe_allow_html=True)
+
+doccano = Image.open('images/dsi_logo.png')
+st.sidebar.image(doccano, caption='', width=100)
+text = """
+**Divisi Research**<br>
+Data Science Indonesia Chapter Jawa Tengah
+"""
+st.sidebar.markdown(text, unsafe_allow_html=True)
 
 ### MENU: HOME ###
 if menu_choice == 'Home':
